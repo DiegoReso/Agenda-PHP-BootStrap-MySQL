fe162fc4fe16c4f41e066322d49a8e3a5b527d10
Diego Reis <diegoreso@me.com>
2023-07-13 14:18:54 -0300

    create show page structure

--- config/process.php
+++ config/process.php
@@ -7,14 +7,36 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
-$query = "SELECT * FROM contacts";
+$id;
 
+if(!empty($_GET)){
+  $id = $_GET['id'];
+}
+
+//retorna o dado de um contato
+if(!empty($id)){
+
+$query = "SELECT * FROM contacts WHERE id = :id";
+
+$stmt = $conn->prepare($query);
+
+$stmt->bindParam(":id", $id);
+
+$stmt->execute();
+//recebe um unico contato atraves do parametro id passado na url
+$contact = $stmt->fetch();
+  
+
+}else{
+//retorna todos os contatos
+$query = "SELECT * FROM contacts";
 
 $stmt = $conn->prepare($query);
 
 $stmt->execute();
-
-
+//recebe todos os contatos
 $contacts = $stmt->fetchAll();
+
+}
 
 ?>
